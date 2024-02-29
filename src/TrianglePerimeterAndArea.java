aab7cc2e993775e9701397fda051ad92dc0b3981
iliyanhadzhiev <iliqnh@gmail.com>
2024-02-29 19:47:11 +0200

    Commit

---
+++ src/TrianglePerimeterAndArea.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TrianglePerimeterAndArea {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is side A of the triangle? ");
+        String firstEntry = scanner.nextLine();
+        double sideA = Double.parseDouble(firstEntry);
+
+        System.out.println("What is side B of the triangle? ");
+        String secondEntry = scanner.nextLine();
+        double sideB = Double.parseDouble(secondEntry);
+
+        System.out.println("What is side C of the triangle? ");
+        String thirdEntry = scanner.nextLine();
+        double sideC = Double.parseDouble(thirdEntry);
+
+        double trianglePerimeter = sideA + sideB + sideC;
+        double s = (trianglePerimeter) / 2;
+        double area = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC)); // Heron's formula
+
+        System.out.println("The perimeter of the triangle is " + trianglePerimeter);
+        System.out.println("The area of the triangle is " + area);
+        System.out.printf("Triangle's area is: %.2f",area);
+    }
+}
